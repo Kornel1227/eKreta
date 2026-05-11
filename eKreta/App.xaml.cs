5761ea61ad34676a4ed26293c701332abd4f51e3
Kornel1227 <tothkornel@turr.hu>
2026-05-11 08:42:38 +0200

    Add project files.

---
+++ eKreta/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace eKreta
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        static string dataBase = "eKreta.db";
+        static string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public static string databasePath = System.IO.Path.Combine(path, dataBase);
+    }
+
+}
