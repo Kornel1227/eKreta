3723c9549c203c792814a2bb460eae9b0ba5e1d0
Kornel1227 <tothkornel@turr.hu>
2026-05-18 08:43:54 +0200

    pusholas0518

---
+++ eKreta/Models/GenericRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eKreta.Models
+{
+    internal class GenericRepository<T> : IGenemicRepository<T> where T : new()
+    {
+        private readonly string _databasePath;
+        public GenericRepository(string databasePath)
+        {
+            _databasePath = databasePath;
+        }
+
+        public List<T> GetAll()
+        {
+            return null;
+        }
+        public void Insert(T item)
+        {
+        }
+        public void Update(T item)
+        {
+        }
+        public void Delete(T item)
+        {
+        }
+    }
+}
